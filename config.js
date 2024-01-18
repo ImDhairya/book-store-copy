9af9863656a9467d75b13cfa4b1a4f2cb13b841f
ImDhairya <98574278+ImDhairya@users.noreply.github.com>
2024-01-18 17:58:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,4 +3,4 @@ export const  PORT = 5555;
 export const mongoDBURL =
   "mongodb+srv://maheshbhatt181995:book123@book-store.ahqk2yj.mongodb.net/?retryWrites=true&w=majority";
 
-#Enter your own password for mongodb Atlas
+// #Enter your own password for mongodb Atlas
